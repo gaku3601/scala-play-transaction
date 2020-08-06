2bb9e3f2bc3fffd2c6d2c9f124a33594c1b4bad8
gaku <pro.gaku@gmail.com>
2020-08-07 05:33:10 +0900

    バリデーション周りの検討

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,35 @@
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
+import javax.inject.{Inject, Singleton}
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import play.api.mvc.{Action, AnyContent, ControllerComponents, _}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+case class PostRequest(body: String, data: String)
+
+object PostRequest {
+  // バリデーションを実施する。フロントでバリデーションは必須。不正な操作で送られた場合、ここで弾く程度のものと認識しておく。
+  implicit val reads: Reads[PostRequest] = (
+    (__ \ "post").read[String].filter(JsonValidationError("必須入力です"))(x => !x.isEmpty) and
+      (__ \ "test" \ "data").read[String].filter(JsonValidationError("必須入力です"))(x => !x.isEmpty)
+    ) (PostRequest.apply _)
+}
+
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def post: Action[JsValue] = Action(parse.json) { implicit request =>
+    request.body.validate[PostRequest].fold(
+      error => {
+        BadRequest(JsError.toJson(error))
+      },
+      postRequest => {
+        Ok(Json.toJson("ok"))
+      }
+    )
   }
 }
