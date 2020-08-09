5413e96b924d74459694b67fba560612f4685952
gaku <pro.gaku@gmail.com>
2020-08-09 17:16:22 +0900

    TODO消化

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -8,13 +8,14 @@ import scala.concurrent._
 
 @Singleton
 class ErrorHandler extends HttpErrorHandler {
-  // TODO: clientErrorについて調査する
+  //クライアントエラー、つまり4xxシリーズのエラーが発生すると呼び出されます。
   def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     Future.successful(
       Status(statusCode)("A client error occurred: " + message)
     )
   }
 
+  //サーバーエラーが発生すると呼び出されます
   def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     Future.successful(
       InternalServerError(Json.obj("error" -> exception.getMessage))
